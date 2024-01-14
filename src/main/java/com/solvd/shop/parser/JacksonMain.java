6c6ab7c7ff630f9e035481cc01cc490af0f84afd
Artem Halushka <ahalushka.laba@solvd.com>
2024-01-14 01:11:44 -0800

    builder impl

--- src/main/java/com/solvd/shop/parser/JacksonMain.java
+++ src/main/java/com/solvd/shop/parser/JacksonMain.java
@@ -22,22 +22,15 @@ public class JacksonMain {
 
     public static void main(String[] args) {
         try {
-            Order order = new Order();
-            order.setOrderId(1);
             SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
             java.util.Date utilDate = dateFormat.parse("2024-01-01");
             java.sql.Date date = new java.sql.Date(utilDate.getTime());
-            order.setDate(date);
             Status status = new Status(1, "ordered");
-            order.setStatus(status);
-            order.setOrderTotal(150.6);
             Country country1 = new Country(1, "USA");
             City city1 = new City(1, "Seattle", country1);
             Address address1 = new Address(1, "150th", "", "9999", city1);
             Buyer buyer = new Buyer(1, "John Smith", "999999999", address1);
-            order.setBuyer(buyer);
             Employee employee = new Employee(1, "Michael Merser", new Position(1, "Sales manager", 10000));
-            order.setEmployee(employee);
             List<Product> products = new ArrayList<>();
             City city2 = new City(2, "California", country1);
             Address address2 = new Address(2, "222 th ne", "building A", "7777", city2);
@@ -47,7 +40,7 @@ public class JacksonMain {
             Product product2 = new Product(2, "Airpods pro", 50.99, supplier, category, 1);
             products.add(product1);
             products.add(product2);
-            order.setProducts(products);
+            Order order = new Order.Builder(1, buyer, products).orderTotal(150).date(date).employee(employee).status(status).build();
             //parser
             String path = "src/main/resources/order.json";
 
